7d063ab197e60b0aa097dc67c52963507cb8fc51
Daniel Worku <danielworku@fb.com>
2018-07-10 19:57:17 -0700

    Added gh-pages plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,6 @@ module.exports = {
     title: 'Ethiopians In Tech',
     language: 'en',
   },
+  pathPrefix: '/ethiopiansintech.org',
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 };
